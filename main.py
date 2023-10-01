8bcf9968b868d07364b43f202603ac11b4f3c06c
Cigfan <anisenyst@yandex.ru>
2023-10-01 21:05:55 +0300

    pythontry

---
+++ main.py
@@ -0,0 +1,18 @@
+# a = 5
+# b = 1.2421
+# c = 'hello'
+
+# # print(f"{a} - {b} - {c}")
+# print("{} - {} - {}".format(a, b, c))
+
+print('Ввелите первую строку')
+n = int(input())
+m = int(input('Введите второе число: '))
+print(n, m, n + m)
+
+# n = 5.88
+# print(n)
+# print(type(n))
+# n = int(n)
+# print(n)
+# print(type(n))
